4c4896f6d9414b8375ed409cb019be0bc58f3c42
Ojaswi Bhardwaj <83720694+ojaswi1234@users.noreply.github.com>
2026-06-12 08:12:51 +0000

    done

--- main.py
+++ main.py
@@ -6,9 +6,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "This is an flask app running in a docker container!"
+    return "bhaad me ja bsdk"
 
 
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
+
+
+
+#https://fuzzy-waddle-6p67vjvv65xf59gv-40763.app.github.dev/api/v1/namespaces/kubernetes-dashboard/services/http:kubernetes-dashboard:/proxy/#/ingressclass?namespace=default
